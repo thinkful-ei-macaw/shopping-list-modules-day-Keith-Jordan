e9a44bb2dbc1b9b9410175f09ff8cd6acaf14fe0
Keith Freitag <keithfreitag@gmail.com>
2020-02-19 09:28:17 -0800

    does this work

--- scripts/store.js
+++ scripts/store.js
@@ -5,6 +5,44 @@ const store = {
   hideCheckedItems: false
 };
 
+
+function findById(id) {
+  return store.items.find(item.id);
+}
+
+function addItem(name) {
+  try {
+    item.validateName(name);
+    store.items.push(item.create(name));
+  } catch (error) {
+}
+
+function findAndToggleChecked(id) {
+  let item = this.findById(id);
+  if (item.checked === false) {
+    item.checked = true;
+  }
+  else {
+    item.checked = false;
+  }
+}
+
+function findAndUpdateName(id, newName) {
+  let item = this.findById(id);
+  try {
+    item.validateName(newName);
+    store.items.push(item.create(newName));
+  } catch (error) {
+    console.log(`Cannot update name: ${error.message}`);
+}
+
+function findAndDelete(id) {
+  let item = this.findById(id);
+  this.items.filter(item);
+}
+
+
+
 export default {
 items: store.items,
 hideCheckedItems: store.hideCheckedItems
